e523a473fbcfc572d8a866f78b3b3428d27c6846
rania <raniaessa651@gmail.com>
2025-06-12 04:27:21 +0300

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,7 +46,7 @@ exports.login = async (req, res) => {
       return res.status(400).json({ message: "Invalid credentials" });
 
     const token = jwt.sign({ id: user._id, role: user.role }, JWT_SECRET, {
-      expiresIn: "7d",
+      expiresIn: "7d", 
     });
 
     res.json({
