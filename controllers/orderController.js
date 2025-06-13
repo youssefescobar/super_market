27972338cc3860e899caa7637ace37a73909afd0
rania <raniaessa651@gmail.com>
2025-06-13 20:24:04 +0300

    change order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,9 +4,11 @@ const Order = require("../models/orderSchema");
 exports.getPaidOrders = async (req, res ) => {
     try {
 
-        const orders = await Order.find({isPaid : true })
-        .populate('userId',' name email ')
-        .populate('items.productId', ' name price');
+       const orders = await Order.find({ isPaid: true })
+  .sort({ createdAt: -1 })           // newest first
+  .limit(20)                         // only get recent 20 orders
+  .populate('userId', 'name email')
+  .populate('items.productId', 'name price');
 
         res.status(201).json({orders});
        
